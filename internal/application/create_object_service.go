334c6623177226152fccee86498928878da3fa61
jibaru <ignacioruedaboada@hotmail.com>
2023-10-17 17:52:55 -0500

    feat: update create object service

--- internal/application/create_object_service.go
+++ internal/application/create_object_service.go
@@ -4,24 +4,27 @@ import (
 	"github.com/google/uuid"
 	"github.com/jibaru/gostore/internal/domain/entities"
 	"github.com/jibaru/gostore/internal/domain/repositories"
+	"github.com/jibaru/gostore/internal/shared"
 	"io"
 	"mime/multipart"
-	"os"
 	"path/filepath"
 )
 
 type CreateObjectService struct {
 	bucketRepository repositories.BucketRepository
 	objectRepository repositories.ObjectRepository
+	filesystem       shared.Filesystem
 }
 
 func NewCreateObjectService(
 	bucketRepository repositories.BucketRepository,
 	objectRepository repositories.ObjectRepository,
+	filesystem shared.Filesystem,
 ) *CreateObjectService {
 	return &CreateObjectService{
 		bucketRepository,
 		objectRepository,
+		filesystem,
 	}
 }
 
@@ -40,7 +43,8 @@ func (serv *CreateObjectService) Do(
 
 	extension := filepath.Ext(file.Filename)
 
-	dst, err := os.Create("./storage/" + bucket.ID + "/" + objectID + extension)
+	// dst, err := os.Create("./storage/" + bucket.ID + "/" + objectID + extension)
+	dst, err := serv.filesystem.MakeFileOnPath(objectID+extension, "/"+bucket.ID)
 	defer dst.Close()
 	if err != nil {
 		return nil, err
